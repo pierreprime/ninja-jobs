66daa61ea4f52343df3fa1602da0ef57b15e62e5
pvermeil <pvermeil@smag.tech>
2021-04-01 13:47:43 +0200

    final commit for ninja jobs

--- src/views/jobs/Jobs.vue
+++ src/views/jobs/Jobs.vue
@@ -1,12 +1,17 @@
 <template>
   <h1>Jobs</h1>
-  <div v-for="job in jobs" :key="job.id" class="job">
-    <router-link :to="{
+  <div v-if="jobs.length">
+    <div v-for="job in jobs" :key="job.id" class="job">
+      <router-link :to="{
       name: 'JobDetails',
       params: {id: job.id}
     }">
-      <h2>{{ job.title }}</h2>
-    </router-link>
+        <h2>{{ job.title }}</h2>
+      </router-link>
+    </div>
+  </div>
+  <div v-else>
+    <p>Loading jobs...</p>
   </div>
 </template>
 
@@ -14,18 +19,20 @@
 export default {
   data() {
     return {
-      jobs: [
-        {title: 'Ninja UX designer', id: 1, details: 'abcdef'},
-        {title: 'Ninja LEGO builder', id: 2, details: 'abcdef'},
-        {title: 'Ninja PHP destroyer', id: 3, details: 'abcdef'},
-      ]
+      jobs: []
     }
+  },
+  mounted() {
+    fetch('http://localhost:3000/jobs')
+        .then(response => response.json())
+        .then(data => this.jobs = data)
+        .catch(error => console.log(error.message));
   }
 }
 </script>
 
 <style>
-.job h2{
+.job h2 {
   background: #f4f4f4;
   padding: 20px;
   border-radius: 10px;
@@ -34,10 +41,12 @@ export default {
   cursor: pointer;
   color: #444;
 }
-.job h2:hover{
+
+.job h2:hover {
   background: #ddd;
 }
-.job a{
+
+.job a {
   text-decoration: none;
 }
 </style>
